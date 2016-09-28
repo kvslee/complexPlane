d9843edece0175582629defd666a088ea0b9393e
Justin Dressel <dressel@chapman.edu>
2016-09-28 00:39:05 -0700

    Fixed typos

--- abscplane.py
+++ abscplane.py
@@ -24,7 +24,7 @@ expect from each other, it devolves into chaos.
 class AbsComplexPlane(ABC):
     """Abstract base class for complex plane.
     
-    A complex plane is a 2x2 grid of complex numbers, having
+    A complex plane is a 2D grid of complex numbers, having
     the form (x + y*1j), where 1j is the unit imaginary number in
     Python, and one can think of x and y as the coordinates for
     the horizontal axis and the vertical axis of the plane, 
@@ -37,15 +37,15 @@ class AbsComplexPlane(ABC):
     this course, so we wish to have an abstract interface that
     is independent of any particular implementation.
     
-    In addition to generating the 2x2 grid of numbers (x + y*1j),
+    In addition to generating the 2D grid of numbers (x + y*1j),
     we wish to easily support transformations of the plane with
     an arbitrary function f. Done properly, the attribute self.plane
-    should store a 2x2 grid of numbers f(x + y*1j) such that the
+    should store a 2D grid of numbers f(x + y*1j) such that the
     parameter x ranges from self.xmin to self.xmax with self.xlen
     total points, while the parameter y ranges from self.ymin to
     self.ymax with self.ylen total points. By default, the function
-    f should be the identity function id, which does nothing to
-    the bare complex plane.
+    f should be the identity function (lambda x : x), which does 
+    nothing to the bare complex plane.
     
     Attributes:
         xmax (float) : maximum horizontal axis value
@@ -67,7 +67,8 @@ class AbsComplexPlane(ABC):
     ylen  = NotImplemented
     # The implementation type of plane is up to the user
     plane = NotImplemented
-    # Note that the default f should be the identity: id
+    # Note that the default f should be the identity: 
+    # (lambda x : x)
     f     = NotImplemented
     
     # The @abstractmethod "decorator" forces all subclasses
